b762d9abc49d842d3c549914fba49f7aaf97b517
서경호 <seogyeongho@seogyeonghoui-iMac.local>
2025-04-21 14:48:36 +0900

    add CopareValue on ComposeActivity

--- app/src/main/java/com/example/internandroid/ComposeActivity1.kt
+++ app/src/main/java/com/example/internandroid/ComposeActivity1.kt
@@ -1,18 +1,34 @@
 package com.example.internandroid
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class ComposeActivity1 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +38,17 @@ class ComposeActivity1 : ComponentActivity() {
         var It = intent.getStringExtra("InputText")
 
         setContent {
-            StopActivityButton(onBackPressedDispatcher, It)
+            Surface (
+                modifier = Modifier.fillMaxSize()
+            ){
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ){
+                    StopActivityButton (onBackPressedDispatcher, It)
+                    CompareValue(applicationContext)
+                }
+            }
         }
     }
 }
@@ -30,13 +56,70 @@ class ComposeActivity1 : ComponentActivity() {
 @Composable
 fun StopActivityButton (onBackPressedDispatcher: OnBackPressedDispatcher, It: String?) {
     Column (
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(text = It.toString())
         Button(onClick = { onBackPressedDispatcher.onBackPressed() }) {
             Text(text = "Go SecondActivity")
+        }
+    }
+}
+
+@Composable
+fun CompareValue(applicationContext: Context) {
+    var result: Boolean = false
+    var value1 by remember { mutableStateOf(TextFieldValue()) }
+    var value2 by remember { mutableStateOf(TextFieldValue()) }
+
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            TextField(
+                value = value1,
+                onValueChange = { value1 = it },
+                label = { Text(text = "Value1") },
+                singleLine = true,
+                modifier = Modifier.width(width = 100.dp)
+            )
+            TextField(
+                value = value2,
+                onValueChange = { value2 = it },
+                label = { Text(text = "Value2") },
+                singleLine = true,
+                modifier = Modifier.width(width = 100.dp)
+            )
+        }
+        Button(onClick = {
+            var result = false
+            if (value1 == value2) result = true
+            else result = false
+
+            Toast.makeText(applicationContext, result.toString(), Toast.LENGTH_SHORT).show()
+        }) {
+            Text(text = "Compare Values")
+        }
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun DefalutPreview() {
+    MaterialTheme {
+        Surface (
+            modifier = Modifier.fillMaxSize()
+        ){
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ){
+                StopActivityButton(onBackPressedDispatcher = OnBackPressedDispatcher(), "It")
+//                CompareValue()
+            }
         }
     }
 }
